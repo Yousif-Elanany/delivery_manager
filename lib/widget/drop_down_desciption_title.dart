9d2710fdc3abc11087bf8aedc98eeda3918349d6
Your Name <you@example.com>
2024-01-02 16:34:04 +0200

    Some Sol of Erors

--- lib/widget/drop_down_desciption_title.dart
+++ lib/widget/drop_down_desciption_title.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class dropdowndecriptiontitle extends StatelessWidget {
-  String Title;
-  dropdowndecriptiontitle(this.Title);
+  String title;
+  dropdowndecriptiontitle(this.title);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(top: 16),
+        margin: const EdgeInsets.only(top: 16),
         child: Text(
-          Title,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ));
   }
 }
